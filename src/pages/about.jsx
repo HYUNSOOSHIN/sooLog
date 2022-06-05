98cce52c629513489af118b590daec5ed426b53d
HYUNSOOSHIN <shs0655@gmail.com>
2022-06-06 02:14:59 +0900

    pages 폴더 정리

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const AboutPage = () => (
+    <Layout>
+        <Seo title="Blog" />
+        <div id="container-about">포트폴리오 페이지입니다.</div>
+    </Layout>
+);
+
+export default AboutPage;
---
+++ src/pages/blog.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const BlogPage = () => (
+    <Layout>
+        <Seo title="Blog" />
+        <div id="container-blog">블로그 페이지입니다.</div>
+    </Layout>
+);
+
+export default BlogPage;
