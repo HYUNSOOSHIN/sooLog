47a90ab91cdcd27f5e429a790035a8e3d172e2fe
HYUNSOOSHIN <shs0655@gmail.com>
2022-06-25 21:08:11 +0900

    copyright 텍스트 수정

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -26,7 +26,7 @@ const Layout = ({ children }) => {
         <>
             <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
             <main>{children}</main>
-            <footer>HYUNSOOSHIN PAGE &copy; HYUNSOOSHIN. 2022</footer>
+            <footer>&copy; HYUNSOOSHIN</footer>
         </>
     );
 };
