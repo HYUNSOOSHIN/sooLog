ae8e93a4a2b283ae0862c759abb955af245c5e81
HYUNSOOSHIN <shs0655@gmail.com>
2022-07-21 20:56:51 +0900

    [2022/07/21]

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,16 @@ import * as React from "react";
 import { Link } from "@reach/router";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import img_hyunsoo from "/static/images/hyunsoo.jpg";
 
 const IndexPage = () => (
     <Layout>
         <Seo title="Home" />
         <div id="container-home">
             <div className="logo-view">
-                <Link className="logo" to="/resume"></Link>
+                <Link className="logo" to="/resume">
+                    <img src={img_hyunsoo} />
+                </Link>
             </div>
             <h1>HYUNSOOSHIN</h1>
             <p>안녕하세요. 신현수입니다.</p>
