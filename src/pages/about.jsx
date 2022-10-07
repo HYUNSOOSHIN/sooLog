2f4acab6a42d7716bc9919e6222e45b1ab82f48b
HYUNSOOGOD <shs0655@fgb.ai>
2022-10-07 17:33:52 +0900

    [2022/10/07]

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import img_hyunsoo from "/static/images/hyunsoo.jpg";
 
 const AboutPage = () => (
     <Layout>
         <Seo title="About" />
         <div id="container-about">
-            <h1>제목입니다!!</h1>
-            <p className="">내용입니다!!</p>
-            <img src="" alt="about img" />
+            <h1>HYUNSOOSHIN PAGE</h1>
+            <p className="">소개하는 페이지</p>
+            <img src={img_hyunsoo} alt="about img" />
         </div>
     </Layout>
 );
